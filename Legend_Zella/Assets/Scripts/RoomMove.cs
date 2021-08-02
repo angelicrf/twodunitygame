792161eeba495d5156d112950704b57c3419651f
Your Name <angelicrf@gmail.com>
2021-08-02 09:46:18 -0700

    text - player transition fixed

--- Legend_Zella/Assets/Scripts/RoomMove.cs
+++ Legend_Zella/Assets/Scripts/RoomMove.cs
@@ -16,22 +16,17 @@ public class RoomMove : MonoBehaviour
     public GameObject newTextObj;
     public Rigidbody2D mvRigid;
     private PlayerMouvment playerMouvement;
-    private string newItem;
-    private Vector2 holdMins;
     private Vector2 dspMins;
     private Vector2 dspMaxs;
     private Camera camera2;
     private Vector2 camPos;
-    private bool isPooled;
-    
+    private int countPool = 0;
     void Start()
     { 
         playerMouvement = GetComponent<PlayerMouvment>();
         
         mvRigid = playerMouvement.plRigid;
-        
-        //GetComponent<Rigidbody2D>();
-              
+        //GetComponent<Rigidbody2D>();            
     }
 
     void FixedUpdate()
@@ -50,49 +45,48 @@ public class RoomMove : MonoBehaviour
         //Debug.Log("Constructor Called2..." + dspMins);
         //Debug.Log("constructor called.." +  dspMaxs);
         //set camera position
-        
         OnTriggerEnter2D();
-       
     }
     private void OnTriggerEnter2D(){
-            
             Debug.Log("collision happened...");
              //playerMouvement.transform.position += playerChange;
             if (textNeeded){
                  StartCoroutine (displayText());
             }
-        
     }
     private IEnumerator displayText(){
         camPos = camera2.transform.position;
         newTextObj.SetActive(true);
          Debug.Log("CamposX-Y is " + camPos);
          if(camPos.x >= 8){
-           
              replaceText = "Pool Area";
-             Debug.Log("ReplaceText is " + replaceText);
              placeText.text = replaceText;
-             chnageAllPos();
-             // a bool similar to: 
-             textNeeded = false;
+             //if condition
+             if(countPool == 0){
+             changeAllPos();
              yield return new WaitForSeconds(4f);
              newTextObj.SetActive(false);
              textNeeded = true;
-         
+             }else{
+              textNeeded = true;
+              yield return new WaitForSeconds(4f);
+              newTextObj.SetActive(false);
+             }             
 
         }else{
-        
             placeText.text = "Home Area";
             yield return new WaitForSeconds(4f);
             newTextObj.SetActive(false);
         } 
    }
-   private void chnageAllPos(){
+   private void changeAllPos(){ 
         GameObject.Find("Main Camera").GetComponent<Camera>().enabled = false;
-             GameObject.Find("Player").GetComponent<PlayerMouvment>().enabled = false;
-             GameObject.Find("Main Camera").GetComponent<Camera>().transform.position = new Vector3(12 , 6, -10);
-             GameObject.Find("Player").GetComponent<PlayerMouvment>().plRigid.transform.position = new Vector3(12, 6, -6);
-             GameObject.Find("Player").GetComponent<PlayerMouvment>().enabled = true;
-             GameObject.Find("Main Camera").GetComponent<Camera>().enabled = true;
+        GameObject.Find("Player").GetComponent<PlayerMouvment>().enabled = false;
+        GameObject.Find("Main Camera").GetComponent<Camera>().transform.position = new Vector3(12 , 6, -10);
+        GameObject.Find("Player").GetComponent<PlayerMouvment>().plRigid.transform.position = new Vector3(12, 6, -6);
+        GameObject.Find("Player").GetComponent<PlayerMouvment>().enabled = true;
+        GameObject.Find("Main Camera").GetComponent<Camera>().enabled = true;
+        textNeeded = false;
+        countPool++;
    }
 }
