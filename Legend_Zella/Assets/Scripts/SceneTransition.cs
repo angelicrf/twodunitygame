3870292486b4503a4f5b23a089160bf18e2f8c71
Your Name <angelicrf@gmail.com>
2021-09-02 09:06:55 -0700

    Fading effect SenceTransition

--- Legend_Zella/Assets/Scripts/SceneTransition.cs
+++ Legend_Zella/Assets/Scripts/SceneTransition.cs
@@ -9,13 +9,31 @@ public class SceneTransition : MonoBehaviour
    public Vector2 plTransit;
    public VectorTransitValue vcTransVal;
    public bool doPass;
+   public GameObject fadedEffect;
+   public float fadeWaiteDur;
+
+    public void Awake(){
+        if(fadedEffect != null){
+            GameObject fdEffect = Instantiate(fadedEffect, Vector2.zero , Quaternion.identity);
+            Destroy(fdEffect , 1);
+        }
+    }
    void OnTriggerEnter2D(Collider2D other){
        if(other.CompareTag("Player") && ! other.isTrigger){
             doPass = true;
             vcTransVal.isTransited = doPass;
             vcTransVal.transitionValue = plTransit;
-            SceneManager.LoadScene(sceneToDisplay);
-            
+            StartCoroutine(FadeOut());         
        }
+   }
+   private IEnumerator FadeOut(){
+      if(fadedEffect != null){
+          Instantiate(fadedEffect, Vector2.zero, Quaternion.identity);
+          yield return new WaitForSeconds(fadeWaiteDur);
+          AsyncOperation asyncOpt = SceneManager.LoadSceneAsync(sceneToDisplay);
+          while(! asyncOpt.isDone){
+              yield return null;
+          }
+      }
    }
 }
