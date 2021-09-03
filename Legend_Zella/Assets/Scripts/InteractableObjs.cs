5bb59eb0a4aff7863d4ebcb5d0e1244b0d1f85c3
Your Name <angelicrf@gmail.com>
2021-09-03 11:48:51 -0700

    Tresure Box

---
+++ Legend_Zella/Assets/Scripts/InteractableObjs.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableObjs : MonoBehaviour
+{
+    public Signal textMarkSignal;
+
+    public bool isDialogActive;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other){
+
+        if(other.name == "Player"){  
+            //textMarkSignal.TestSignal();
+            textMarkSignal.ReadSignals();
+            //TextFunc();
+        }    
+    }
+    private void OnTriggerExit2D(Collider2D other){
+        if(other.name == "Player"){ 
+            textMarkSignal.ReadSignals();
+           /*  isDialogActive = false;
+            dialogBox.SetActive(false); */
+        }
+    }
+}
--- Legend_Zella/Assets/Scripts/Sign.cs
+++ Legend_Zella/Assets/Scripts/Sign.cs
@@ -3,25 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Sign : MonoBehaviour
+public class Sign : InteractableObjs
 {
     public GameObject dialogBox;
     public Text dialogText;
     public string textDisplay; 
-    public bool isDialogActive;
-    public Signal textMarkSignal;
-    
-    private void OnTriggerEnter2D(Collider2D other){
-
-        if(other.name == "Player"){  
-            //textMarkSignal.TestSignal();
-            textMarkSignal.ReadSignals();
-            //TextFunc();
-        }    
-    }
+   
     private void OnTriggerExit2D(Collider2D other){
         if(other.name == "Player"){ 
             textMarkSignal.ReadSignals();
+            // need to create a  new dialog box 
            /*  isDialogActive = false;
             dialogBox.SetActive(false); */
         }
