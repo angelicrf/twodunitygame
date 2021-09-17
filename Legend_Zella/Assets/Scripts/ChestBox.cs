8c77624da38145f6fb3cf4303ab1d8061d4b76e3
Your Name <angelicrf@gmail.com>
2021-09-17 08:18:54 -0700

    tresurebox fixed trigger once

--- Legend_Zella/Assets/Scripts/ChestBox.cs
+++ Legend_Zella/Assets/Scripts/ChestBox.cs
@@ -9,16 +9,14 @@ public class ChestBox : MonoBehaviour
     public TresureItem gmItem;
     private GameObject interObj;
     private Animator animator;
-    private bool isCollision;
     private Rigidbody2D otherPlRigid;
+
     
     void Start()
     {
-        isCollision = false;
         interObj = GameObject.Find("Player").transform.GetChild(5).gameObject;
         animator = GetComponent<Animator>(); 
-        gmInventory.itemCount = 0; 
-     
+        //gmInventory.itemCount = 0;      
         gmInventory.isItem = false;
         //gmItem.isOpen = false;
     }
@@ -29,20 +27,10 @@ public class ChestBox : MonoBehaviour
     }
      private void OnTriggerEnter2D(Collider2D other){
 
-        if(other.name == "Player" && ! gmItem.initialIsOpen){  
-
-           isCollision = true;
+        if(other.name == "Player" && ! gmItem.initialIsOpen && ! other.isTrigger){  
            otherPlRigid  = other.GetComponent<Rigidbody2D>();
-        }    
-    }
-  /*   private void OnTriggerExit2D(Collider2D other){
-
-        if(other.name == "Player"){
-        }
-    } */
-      private void OnCollisionEnter2D(Collision2D other){
-           if(isCollision && Input.anyKey){
-               if(Input.GetKey("right") || Input.GetKey("left") || Input.GetKey("up") || Input.GetKey("down")){
+       
+            if(Input.GetKey("right") || Input.GetKey("left") || Input.GetKey("up") || Input.GetKey("down")){
                //interObj.textMarkSignal.ReadSignals();
                animator.SetTrigger("TrBox");
                animator.SetBool("isActive", true);
@@ -53,21 +41,20 @@ public class ChestBox : MonoBehaviour
                    interObj.SetActive(true);              
                    gmInventory.isItem = true;
                    gmInventory.getItems();
-                   isCollision = false;
                }
              }
-           }
-        }
-        private void OnCollisionExit2D(Collision2D other){
-            if(!isCollision){
-               otherPlRigid.GetComponent<PlayerMouvment>().currentPlState = PlayerMouvment.PlayerState.idle; 
+        }    
+    }
+    private void OnTriggerExit2D(Collider2D other){
+
+        if(other.name == "Player" && !other.isTrigger){
+            otherPlRigid.GetComponent<PlayerMouvment>().currentPlState = PlayerMouvment.PlayerState.idle; 
                interObj.SetActive(false);
                animator.SetBool("isActive", false);     
             //interObj.textMarkSignal.ReadSignals();
                 gmItem.items.SetActive(false);
                 gmInventory.isItem = false;
-                //gmItem.isOpen = false;
-            }
         }
+    }
 
 }
